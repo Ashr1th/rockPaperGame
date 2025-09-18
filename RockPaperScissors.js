73850ed065bd6b50b5a0e2508ce209edd975ac81
Ashr1th <115359037+Ashr1th@users.noreply.github.com>
2025-09-18 18:50:35 +0530

    some minor changes

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,30 +1,45 @@
 "use strict"
 
-let answer = prompt("Enter rock(r) paper(p) or scissor(s)")
-
-let ran = (Math.random() * 100) + 1;
-
-let term;
-
-if (ran >= 0 && ran <= 33) {
-  term = 'r';
-} else if (ran >= 33 && ran <= 66) {
-  term = "p";
-} else {
-  term = "s";
+function getHumanChoice() {
+  let answer = prompt("Enter rock(r) paper(p) or scissor(s)")
+  return answer;
 }
 
-if (answer === term) {
-  alert(`i choose ${term}`);
-  alert("tie");
-} else if ((answer === "r" && term === "s") || (answer === "p" && term === "r") || (answer === "s" && term === "p")) {
-  alert(`i choose ${term}`);
-  alert("you won");
-} else if (answer === "" || answer === null) {
-  // alert(`i choose ${term}`);
-  alert("pick a proper thing!!!!!!!!!!!!");
+
+showWinner(computerScore);
+
+function getComputerChoice() {
+  let ran = (Math.random() * 100) + 1;
+  console.log("Ran =", ran);
+  let term;
+  if (ran >= 0 && ran <= 33) {
+    term = 'r';
+  } else if (ran >= 33 && ran <= 66) {
+    term = "p";
+  } else {
+    term = "s";
+  }
+  return term;
 }
-else {
-  alert(`i choose ${term}`);
-  alert("you lose");
+
+
+function showWinner() {
+  const term = getComputerChoice();
+  const answer = getHumanChoice();
+  if (answer === term) {
+    alert(`i choose ${term}`);
+    alert("tie");
+  } else if ((answer === "r" && term === "s") || (answer === "p" && term === "r") || (answer === "s" && term === "p")) {
+    alert(`i choose ${term}`);
+    alert("you won");
+  } else if (answer === "" || answer === null) {
+    // alert(`i choose ${term}`);
+    alert("pick a proper thing!!!!!!!!!!!!");
+  }
+  else {
+    computerScore++;
+    alert(`i choose ${term}`);
+    alert("you lose");
+  }
 }
+
